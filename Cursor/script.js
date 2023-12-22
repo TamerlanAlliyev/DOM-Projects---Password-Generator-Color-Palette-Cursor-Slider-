d08b26b6feee4ad451cc1b0803630bf75c8ed78b
TamerlanAlliyev <tamerlanma@code.edu.az>
2023-12-22 12:20:16 +0400

    a

--- Cursor/script.js
+++ Cursor/script.js
@@ -3,27 +3,37 @@ const cursorInner = document.querySelector('.cursor--inner')
 
 const btn = document.querySelector('.btn')
 
-const {width:cWidth,height:cHeight,borderRadius:cBorderRadius} = window.getComputedStyle(cursorOuter)
+const { width: cWidth, height: cHeight, borderRadius: cBorderRadius, margin:cMargin } = window.getComputedStyle(cursorOuter)
 
 window.addEventListener('mousemove', (event) => {
     const { x, y, target } = event
 
+    const { x: btnCoordsX, y: btnCoordsY } = btn.getBoundingClientRect();
     // const cordinant = btn.getBoundingClientRect();
     // console.log(cordinant);
 
     cursorOuter.style.transform = `translate(${x}px,${y}px)`
     cursorInner.style.transform = `translate(${x}px,${y}px)`
 
-    if(target.classList.contains('btn')){
+    if (target.classList.contains('btn')) {
         // const buttonStyle = window.getComputedStyle(btn)
-        const {width: btnWidth,height:btnHeight,borderRadius:btnBorderRadius} = window.getComputedStyle(btn)
+        const { width: btnWidth,
+            height: btnHeight,
+            borderRadius: btnBorderRadius
+        } = window.getComputedStyle(btn)
+
         cursorOuter.style.width = btnWidth
         cursorOuter.style.height = btnHeight
         cursorOuter.style.borderRadius = btnBorderRadius
-    }else{
+
+        cursorOuter.style.transform = `translate(${btnCoordsX}px,${btnCoordsY}px)`
+        cursorOuter.style.margin=0;
+
+    } else {
         cursorOuter.style.width = cWidth
         cursorOuter.style.height = cHeight
         cursorOuter.style.borderRadius = cBorderRadius
+
     }
 })
 
